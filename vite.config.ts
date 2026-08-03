46c00a9767bba2241b186bf9ecf30d3a8369e493
Renasoz <renasgs10@hotmail.com>
2026-08-04 00:17:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/glossydetailsnl/',
+ 
   server: {
     host: "::",
     port: 8080,
